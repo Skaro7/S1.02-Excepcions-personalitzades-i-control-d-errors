526edbc817a79259a14772bf2812a61eedcbca9f
Skaro7 <marc_tau72@hotmail.com>
2025-12-15 12:55:29 +0100

    "DOCS: Created class and added functionality"

---
+++ src/Sale.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sale{
+    private List<Product> products;
+    private double totalPrice;
+
+    public Sale() {
+        this.products = new ArrayList<>();
+        this.totalPrice = 0.0;
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+        totalPrice += product.getPrice();
+    }
+
+    public void calculateTotal() throws EmptySaleException {
+        if (products.isEmpty()) {
+           throw new EmptySaleException();
+        }
+        totalPrice = 0.0;
+        for (Product p : products) {
+            totalPrice += p.getPrice();
+        }
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+}
